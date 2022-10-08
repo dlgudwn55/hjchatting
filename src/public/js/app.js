85ab450fcc7537372f83966b1913722af904bef5
dlgudwn55 <dkrlekeheoek@naver.com>
2022-10-08 16:23:29 +0900

    design html

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,32 @@
-alert("hello");
+const socket = io();
+
+const welcomeDiv = document.getElementById("welcome");
+const loginForm = document.getElementById("login");
+const roomDiv = document.getElementById("room");
+const messageForm = document.getElementById("send-message");
+const exitBtn = document.getElementById("exit-button")
+
+let nickname;
+let roomName;
+
+roomDiv.hidden = true;
+
+function handleLoginSubmit(event) {
+    event.preventDefault();
+    const submittedNickname = loginForm.querySelector("#nickname");
+    const submittedRoomName = loginForm.querySelector("#room-name");
+    nickname = submittedNickname.value;
+    roomName = submittedRoomName.value;
+    socket.emit("enter_room", nickname, roomName);
+    welcomeDiv.hidden = true;
+    roomDiv.hidden = false;
+
+    messageForm.addEventListener("submit", handleMessageSubmit);
+    exitBtn.addEventListener("click", handleExit)
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+}
+
+loginForm.addEventListener("submit", handleLoginSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,18 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000);
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (nickname, roomName) => {
+        socket.join(roomName);
+        console.log(socket);
+    })
+})
+
+httpServer.listen(3000, handleListen);
